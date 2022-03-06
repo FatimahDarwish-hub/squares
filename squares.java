f56d33143da61538639e87b725f710db2033b4e1
Fatimah Alshaikhi <51248702+FatimahDarwish-hub@users.noreply.github.com>
2022-03-06 21:42:12 +0300

    Create squares.java

---
+++ squares.java
@@ -0,0 +1,12 @@
+
+public class squares {
+
+    public static void main(String[] args) {
+        System.out.println("Squares of the numbers 1 to 20 inclusive in ascending order");
+        for ( int j =1 ; j<=20; j++)
+        {
+            System.out.println(j + " squared = " + j *j);
+        }
+    }
+    
+}
